43f03df9f8d706e5b934d15295c9991c5a0de242
Flowvers <186988140+Flowvers@users.noreply.github.com>
2025-01-22 20:14:51 -0500

    Create getComputerChoice function

--- RoShamBo.js
+++ RoShamBo.js
@@ -1,1 +1,24 @@
 console.log("Hellow World!");
+// getComputerChoice function
+    // Generate varaible X = random string rock paper scissors
+    // Output random string
+
+function getComputerChoice() {
+    let randInt = Math.floor(Math.random() * 3);
+    switch (randInt) {
+        case 0:
+            console.log("Rock")
+            break;
+        case 1:
+            console.log("Paper")
+            break;
+        case 2:
+            console.log("Scissors")
+            break;
+        default:
+            console.log("Default")
+            break;
+    }
+}   
+
+getComputerChoice()
